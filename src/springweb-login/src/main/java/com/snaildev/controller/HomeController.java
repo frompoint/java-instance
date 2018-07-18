61b506705c56de51076359de2ba0c662357ea475
zhaoming <zhaoming@mallcoo.cn>
2018-07-18 18:34:06 +0800

    springweb-login

---
+++ src/springweb-login/src/main/java/com/snaildev/controller/HomeController.java
@@ -0,0 +1,14 @@
+package com.snaildev.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/home")
+public class HomeController {
+
+    @RequestMapping("/hello")
+    public String hello() {
+        return "hello1";
+    }
+}
